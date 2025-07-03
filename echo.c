379d95010f4c18868ca2ce28d7bb608a0183f23d
Dhimant-1001 <dhimant.bhuva@students.iiit.ac.in>
2025-07-03 12:05:43 +0530

    changes to echo

--- echo.c
+++ echo.c
@@ -2,19 +2,17 @@
 #include "echo.h"
 
 void echo_command(char **args, int arg_count) {
-    int newline = 1; // By default, add newline at the end
-    int start_index = 0; // Index to start printing arguments from
+    int newline = 1; 
+    int start_index = 0; 
     
-    // Check for -n flag (suppress trailing newline)
     if (arg_count > 0 && args[0] != NULL && strcmp(args[0], "-n") == 0) {
         newline = 0;
-        start_index = 1; // Skip the -n flag when printing
+        start_index = 1; 
     }
     
-    // Print all arguments separated by spaces
     for (int i = start_index; i < arg_count; i++) {
         if (args[i] != NULL) {
-            // Add space before argument if it's not the first one
+            
             if (i > start_index) {
                 printf(" ");
             }
@@ -22,7 +20,6 @@ void echo_command(char **args, int arg_count) {
         }
     }
     
-    // Add newline at the end unless -n flag was specified
     if (newline) {
         printf("\n");
     }
